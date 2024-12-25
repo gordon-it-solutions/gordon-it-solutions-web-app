7b90a1d775b10d2d96b57b781d0129aaa937f024
Scott Gordon <ScottGordon@gordonitsolutions.com>
2024-12-25 18:44:19 -0500

    Update navigation bar with new pages

---
+++ GordonITSolutionsWebApp/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GordonITSolutionsWebApp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
