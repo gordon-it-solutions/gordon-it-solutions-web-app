a77b74a3e8fe47e2db1012b101a7737ec8aecb8a
Scott Gordon <ScottGordon@gordonitsolutions.com>
2024-12-26 14:28:51 -0500

    Add web page content

---
+++ GordonITSolutionsWebApp/Pages/Blog.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace GordonITSolutionsWebApp.Pages
+{
+    public class BlogModel : PageModel
+    {
+        public void OnGet()
+        { // Add any server-side logic needed for the Blog page here 
+        }
+    }
+}
